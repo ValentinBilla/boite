56a1a8f1e4323fb1832b0ec79e706306cc5a2b14
Valentin Billa <valentin.billa@gmail.com>
2024-09-05 11:36:45 +0200

    :poop: Allows *

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from datetime import datetime
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/users")
 async def get_users() -> list[str]:
