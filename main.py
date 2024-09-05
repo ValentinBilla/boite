3a20a632a33b43c54f1e81e72820b00224c3c82e
Valentin Billa <valentin.billa@gmail.com>
2024-09-05 10:59:08 +0200

    :tada: Basic API

---
+++ main.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/users")
+async def get_users() -> list[str]:
+    with open('users', 'r', encoding='utf-8') as f:
+        return list(map(str.strip, f.readlines()))
+
+class Retrieval(BaseModel):
+    hour: datetime
+    name: str
+
+@app.post("/retrievals")
+async def post_retrieval(name: str) -> None:
+    with open('hours.csv', 'a', encoding='utf-8') as f:
+        f.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ', ' + name.strip() + '\n')
+
+@app.get("/retrievals")
+async def get_retrievals() -> list[Retrieval]:
+    with open('hours.csv', 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    retrievals = list(map(line_to_retrieval, lines))
+    retrievals.reverse()
+    return retrievals
+
+def line_to_retrieval(line: str) -> Retrieval:
+    hour, name = line.split(', ')
+    return Retrieval(
+        hour=datetime.strptime(hour, '%Y-%m-%d %H:%M:%S'),
+        name=name.strip()
+    )
